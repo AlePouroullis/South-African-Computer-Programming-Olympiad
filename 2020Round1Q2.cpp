7c03e5a8d5c606bf7f7a74c064ee3e6daf5ad3c2
Alexandros Pouroullis <69683229+AlePouroullis@users.noreply.github.com>
2021-02-11 11:29:40 +0200

    Add files via upload

---
+++ 2020Round1Q2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    long long total_grains_on_black_tiles = 0;
+    long long grains_on_current_tile;
+
+    for(int i = 1; i <= n; i++){
+        if(i == 1)
+            grains_on_current_tile = 1;
+        else
+            grains_on_current_tile = grains_on_current_tile * 2;
+
+        if(i % 2 == 1)
+            total_grains_on_black_tiles += grains_on_current_tile;
+    }
+
+    cout << total_grains_on_black_tiles;
+}
+
+
+
+
+
