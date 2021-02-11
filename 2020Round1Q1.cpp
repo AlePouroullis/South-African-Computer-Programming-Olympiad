dba5052e26bd435241acb6e3127a5ec772218828
Alexandros Pouroullis <69683229+AlePouroullis@users.noreply.github.com>
2021-02-11 11:30:05 +0200

    Update 2020Round1Q1.cpp

--- 2020Round1Q1.cpp
+++ 2020Round1Q1.cpp
@@ -1,28 +1,4 @@
-/*#include <algorithm>
-#include <array>
-#include <cassert>
-#include <chrono>
-#include <cmath>
-#include <cstring>
-#include <functional>
-#include <iomanip>
 #include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <random>
-#include <set>
-#include <sstream>
-#include <vector>
-#define fast ios::sync_with_stdio(false);cin.tie(0);
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double ld;
-#define pii pair<int, int>
-#define vi vector<int>
-#define pb push_back
-*/
-#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -31,8 +7,3 @@ int main(){
     cin >> chickens, cows, bees;
     cout << chickens * 2 + cows * 4 + bees * 6;
 }
-
-
-
-
-
