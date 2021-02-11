7054eb52b5882e7262fbfc438b0bfb318c4f729f
Alexandros Pouroullis <69683229+AlePouroullis@users.noreply.github.com>
2021-02-11 11:25:51 +0200

    Add files via upload

---
+++ 2020Round1Q1.cpp
@@ -0,0 +1,38 @@
+/*#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <vector>
+#define fast ios::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+#define pii pair<int, int>
+#define vi vector<int>
+#define pb push_back
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int chickens, cows, bees;
+    cin >> chickens, cows, bees;
+    cout << chickens * 2 + cows * 4 + bees * 6;
+}
+
+
+
+
+
