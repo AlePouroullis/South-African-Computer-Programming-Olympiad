6664391d673594ec912709b8e1aff968dd7f2ae5
Alexandros Pouroullis <69683229+AlePouroullis@users.noreply.github.com>
2021-02-11 11:57:37 +0200

    Update Q1.cpp

--- 2020/Round 1/Q1.cpp
+++ 2020/Round 1/Q1.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main(){
     int chickens, cows, bees;
-    cin >> chickens, cows, bees;
+    cin >> chickens >> cows >> bees;
     cout << chickens * 2 + cows * 4 + bees * 6;
 }
