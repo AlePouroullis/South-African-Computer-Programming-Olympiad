b74f1b0d26d080f96929e161bff1515086af13cb
Alexandros Pouroullis <69683229+AlePouroullis@users.noreply.github.com>
2021-02-14 00:22:45 +0200

    Create Q1.cpp

---
+++ 2018/Round 1/Q1.cpp
@@ -0,0 +1,17 @@
+// 2018 Round 1
+// Question 1
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int daytime, evening, weekend;
+    cin >> daytime >> evening >> weekend;
+    int total_cost = 0;
+    total_cost += (daytime > 100 ? (daytime - 100)*80 : 0);
+    total_cost += evening * 70;
+    total_cost += 50 * weekend;
+    cout << total_cost << "c" << endl;
+}
