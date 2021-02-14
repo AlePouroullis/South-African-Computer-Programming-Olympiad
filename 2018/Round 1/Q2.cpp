22bb876d19be9e14622cb4dfa9f330f666928213
Alexandros Pouroullis <69683229+AlePouroullis@users.noreply.github.com>
2021-02-14 12:54:49 +0200

    Update Q2.cpp

--- 2018/Round 1/Q2.cpp
+++ 2018/Round 1/Q2.cpp
@@ -6,12 +6,18 @@
 
 using namespace std;
 
+bool isvowel(const char &letter){
+    return (letter == 'a' && letter == 'e' 
+        && letter == 'i'  && letter == 'o' 
+        && letter == 'u');
+}
+
 int main(){
     string input_string;
     getline(cin, input_string);
+
     for(int i = 0; i < input_string.length(); i++){
-        if(input_string[i] != 'a' && input_string[i] != 'e' && input_string[i] != 'i'
-           && input_string[i] != 'o' && input_string[i] != 'u' && input_string[i] != ' '){
+        if(!isvowel(input_string[i]) && input_string[i] != ' '){
             cout << input_string[i] << 'o' << input_string[i];
            }
         else
